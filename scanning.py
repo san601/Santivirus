2098c6d1512c6d73324f4892f308dc5934b96d0f
Nguyễn Huy San <144963803+san601@users.noreply.github.com>
2024-05-12 17:42:58 +0700

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -16,14 +16,13 @@ def badCharHeuristic(string, size):
     Returns:
         A list of size NO_OF_CHARS with the last occurrence of each character in the pattern
     """
-    # Initialize all occurrence as -1
+    # Initialize
     badChar = [-1] * NO_OF_CHARS
 
     # Fill the actual value of last occurrence
     for i in range(size):
         badChar[ord(string[i])] = i
 
-    # return initialized list
     return badChar
 
 
@@ -41,24 +40,15 @@ def check(txt, pat):
     m = len(pat)
     n = len(txt)
 
-    # create the bad character list by calling
-    # the preprocessing function badCharHeuristic()
-    # for given pattern
     badChar = badCharHeuristic(pat, m)
 
-    # s is shift of the pattern with respect to text
     s = 0
     while s <= n - m:
         j = m - 1
 
-        # Keep reducing index j of pattern while
-        # characters of pattern and text are matching
-        # at this shift s
         while j >= 0 and pat[j] == txt[s + j]:
             j -= 1
 
-        # If the pattern is present at current shift,
-        # then index j will become -1 after the above loop
         if j < 0:
             return True
         else:
